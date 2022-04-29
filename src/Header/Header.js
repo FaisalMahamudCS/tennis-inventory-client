f40929227cc3c827557aac1c2d41bd2ed2eae4eb
FaisalMahamudCS <fmfahimcse@gmail.com>
2022-04-30 00:40:33 +0600

    register done

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import {Navbar,Container,Nav} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
+import {useAuthState} from 'react-firebase-hooks/auth'
+import auth from '../firebase.init'
+import { signOut } from 'firebase/auth';
+
 const Header = () => {
+  const [user,loading,error]=useAuthState(auth);
+  const logout=()=>{
+    signOut(auth);
+  }
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" bg="white">
@@ -12,7 +20,13 @@ const Header = () => {
     <Nav className="me-auto">
       <Nav.Link as={Link} className='text-dark' to='/item'>Item</Nav.Link>
       <Nav.Link as={Link} className='text-dark' to='/category'>Category</Nav.Link>
-      <Nav.Link as={Link} className='text-dark' to='/login'>Login</Nav.Link>
+      {
+       user?
+      <button className='text-decoration-none btn btn-link ' onClick={logout}> logout </button>
+    :
+    <Nav.Link as={Link} className='text-dark' to='/login'>Login</Nav.Link>
+      }
+     
   
     </Nav>
     <Nav>
--- src/PrivateAuth/PrivateAuth.js
+++ src/PrivateAuth/PrivateAuth.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../firebase.init';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 
 const PrivateAuth = ({children}) => {
     const [user,loading,error]=useAuthState(auth);
-    
-
-    return (
-       
-
-        <div>
-            
-        </div>
-    );
+    const location=useLocation();
+    if(!user){
+        return <Navigate to='/login' state={{from:location}} replace/>
+    }
+//handle reload error
+if(loading){
+return <LoadingSpinner></LoadingSpinner>
+}
+  
     return children;
 };
 
